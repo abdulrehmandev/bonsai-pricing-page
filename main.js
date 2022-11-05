e708cf7b504288747ba42083c1e9acd7263a4319
Abdul Rehman <beingabdulr@gmail.com>
2022-11-05 05:19:43 -0700

    Add dropdown to header

--- main.js
+++ main.js
@@ -2,10 +2,25 @@
 const navBtn = document.querySelector('.toggle')
 const primaryNav = document.querySelector('.primary-navigation')
 
+// nav menu click
+
 navBtn.addEventListener('click', () => {
   primaryNav.classList.toggle('nav-visible')
 
   navBtn.setAttribute('aria-expanded',
   (navBtn.getAttribute('aria-expanded') === 'false' ? true : false))
+})
 
+// dropdown hover
+
+const dropdownTitle = document.querySelector('.dropdown')
+const dropdownMenu = document.querySelector('.dropdown-menu')
+
+let rotation = 0
+
+dropdownTitle.addEventListener('mousemove', () => {
+  dropdownMenu.classList.toggle('dropdown-visible')
+
+  rotation = rotation == 0 ? 180 : 0
+  document.querySelector('.dropdown-arrow').style.transform = `rotate(${rotation}deg)`
 })
